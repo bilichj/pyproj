6273352e0092f816a09ce039f200c52df3d67f98
Jordan Bilich <bilichj@gmail.com>
2020-07-28 14:22:01 -0400

    updated script

--- pyproj.py
+++ pyproj.py
@@ -2,38 +2,112 @@
 
 import subprocess
 import os
+import argparse
+from pathlib import Path
+import requests
+import sys
+import webbrowser
 
-REPO_ROOT = '/home/jordan/src/github.com/bilichj/'
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('name', type=str, default=None, help='a name for the project')
+parser.add_argument('description', type=str, default=None, help='a brief description')
+parser.add_argument('--public', dest='public', action='store_const',
+                    const=True, default=False,
+                    help='make repository public (default: private)')
 
+
+USERNAME = 'bilichj'
+REPO_ROOT = Path('/home/jordan/src/github.com/bilichj/')
+
+class RepositoryExistsError(Exception):
+    pass
+
+
+def get_remote_repo_names(username):
+    response = requests.get(f'https://api.github.com/users/{username}/repos')
+    return [repo['name'] for repo in response.json()]
+
+args = parser.parse_args()
 os.chdir(REPO_ROOT)
+path = REPO_ROOT.joinpath(args.name)
 
-project_name = input('Project name: ')
+if path.exists():
+    raise FileExistsError('Directory with that project name already exists locally.')
 
+if args.name in get_remote_repo_names(USERNAME):
+    raise RepositoryExistsError('Repository with that name already exists remotely.')
+
+path.mkdir()
+path.joinpath(args.name).mkdir()
+os.chdir(args.name)
+
+print('Initializing repo...')\
 
 try:
-    os.makedirs(f'{project_name}/{project_name}')
-except:
-    raise Exception('Project already exists locally.')
+    subprocess.check_call(['git', 'init'])
+except subprocess.CalledProcessError:
+    print('git init failed, cleaning up...')
+    subprocess.check_call(['rm', '-rf', path])()
+    sys.exit(1)
 
-os.chdir(project_name)
+readme = f'# {args.name}\n{args.description}'
 
-subprocess.check_call(['git', 'init'])
+try:
+    subprocess.check_call(['pipenv', 'install'])
+except subprocess.CalledProcessError:
+    print('pipenv install failed, cleaning up...')
+    subprocess.check_call(['rm', '-rf', path])()
+    sys.exit(1)
 
-project_description = input('Description: ')
 
-readme = f'#{project_name}\n{project_description}'
-print('Initializing repo...')
-
-subprocess.check_call(['pipenv', 'install'])
 with open('README.md', 'w') as f:
     f.write(readme)
 
-subprocess.check_call(['touch', f'{project_name}/__init__.py'])
+try:
+    subprocess.check_call(['touch', f'{args.name}/__init__.py'])
 
-subprocess.check_call(['git', 'add', 'README.md'])
-subprocess.check_call(['git', 'commit', '-m', '"initial commit"'])
+except subprocess.CalledProcessError:
+    print('pipenv install failed, cleaning up...')
+    subprocess.check_call(['rm', '-rf', path])()
+    sys.exit(1)
 
-subprocess.check_call(['hub', 'create'])
-subprocess.check_call(['git', 'push', '--set-upstream', 'origin', 'master'])
+try:
+    subprocess.check_call(['git', 'add', 'README.md'])
+except subprocess.CalledProcessError:
+    print('adding README failed, cleaning up...')
+    subprocess.check_call(['rm', '-rf', path])()
+    sys.exit(1)
 
-print('Success!')
+try:
+    subprocess.check_call(['git', 'add', f'{args.name}/__init__.py'])
+except subprocess.CalledProcessError:
+    print('adding README failed, cleaning up...')
+    subprocess.check_call(['rm', '-rf', path])()
+    sys.exit(1)
+
+try:
+    subprocess.check_call(['git', 'commit', '-m', '"initial commit"'])
+
+except subprocess.CalledProcessError:
+    print('commit failed, cleaning up...')
+    subprocess.check_call(['rm', '-rf', path])()
+    sys.exit(1)
+
+create_command = ['hub', 'create'] + ( [] if args.public else ['-p'])
+
+try:
+    subprocess.check_call(create_command)
+
+except subprocess.CalledProcessError:
+    print('github repo creation failed, cleaning up...')
+    subprocess.check_call(['rm', '-rf', path])()
+    sys.exit(1)
+
+try:
+    subprocess.check_call(['git', 'push', '--set-upstream', 'origin', 'master'])
+except subprocess.CalledProcessError:
+    print('Push failed. Remote repo still exists, keeping local files.')
+    sys.exit(1)
+
+print('Success! Opening VSCode...')
+subprocess.check_call(['open-project', 'args.name'])
